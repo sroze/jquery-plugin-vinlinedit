552e8ecd7f7030b4014b82894b1e8028240f334c
Samuel ROZE <samuel.roze@gmail.com>
2012-03-20 21:01:49 +0100

    Initial project repository.

---
+++ jquery-vinlinedit/src/jquery.vinlinedit.js
@@ -0,0 +1,20 @@
+/*
+* Vinlinedit jQuery Plugin v1.0.0
+*
+* Copyright (c) 2012 Samuel ROZE
+* Licensed under the GPLv3 license.
+*
+*/
+(function($){
+	$.fn.vinlinedit = function(options) {
+		// Merge options with defaults
+		var settings = jQuery.extend({
+			
+		}, options);
+		
+		// For each element, add features.
+		return this.each(function(){
+			
+		});
+	};
+})(jQuery);
