338f153df4c3b16c24014496b541b015781d69d0
Samuel ROZE <samuel.roze@gmail.com>
2012-03-20 23:57:56 +0100

    Add features and markdown README file.

---
+++ jquery-vinlinedit/README.md
@@ -0,0 +1,57 @@
+VInlinEdit jQuery Plugin v1.0.0
+===============================
+Copyright (c) 2012 Samuel ROZE. Licenced under the GPLv3 license.
+
+Overview
+--------
+Vinlinedit is a jQuery plugin that provide an inline edit for text spans. The
+principal attribute is that the editor's style is like vi.
+
+Requirements
+------------
+This plugin requires this tow librairies (which are in the `lib` directory):
+- [jQuery](http://www.jquery.com)
+- [jsAsyncQueue](https://github.com/hagino3000/jsAsyncQueue/) (see [JavaScript: Synchroniser des événements asynchrones (fr)](http://www.d-sites.com/2012/03/20/javascript-synchroniser-des-evenements-asynchrones/) to see an example of use)
+
+How to use
+----------
+In order to use this plugin, you just have to include the JS and CSS files (and, of course, the requirements) which are in the `src` folder.
+Then, you can see the `examples` folder, or this simple example:
+
+	$('span.vinlinedit').vinlinedit(options);
+	
+The `option` argument is an object.
+
+### Options
+There are 3 options at the moment, which are:
+
+<table>
+	<thead>
+		<tr>
+			<td>Option name</td>
+			<td>Value(s)</td>
+			<td>Description</td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>prefix</td>
+			<td>string</td>
+			<td>A string which will prefix the inline editor.</td>
+		</tr>
+		<tr>
+			<td>calback_pre_keypress</td>
+			<td>function (keypressEvent) returns boolean</td>
+			<td>
+				<p>A callback which will be called before keypress processing.</p>
+				<p>It the return value is <code>true</code>, VInlinEdit won't process the event. It may be interessing for extending fonctionnalities such as Enter key, ...</p>
+			</td>
+		</tr>
+		<tr>
+			<td>calback_post_keypress</td>
+			<td>function (keypressEvent)</td>
+			<td>A callback which will be called after keypress processing.</td>
+		</tr>
+	</tbody>
+</table>
+
--- jquery-vinlinedit/src/jquery.vinlinedit.js
+++ jquery-vinlinedit/src/jquery.vinlinedit.js
@@ -9,12 +9,129 @@
 	$.fn.vinlinedit = function(options) {
 		// Merge options with defaults
 		var settings = jQuery.extend({
+			// A prefix for content to edit
+			// @var string
+			prefix: '',
 			
+			// A callback before keypress processing
+			// @var function(keypressEvent) which returns true if catch the event
+			calback_pre_keypress: null,
+			
+			// A callback after keypress processing
+			// @var function(keypressEvent)
+			calback_post_keypress: null
 		}, options);
 		
 		// For each element, add features.
 		return this.each(function(){
+			var span = $(this).attr('tabindex', '0').focus();
+			var rcc_id = null;
 			
+			// Init required Async Queue to sync
+			// the keyboard events.
+			var queue = new AsyncQueue({name : 'keyboard-events'});
+			
+			// Create the needed html
+			var initial_content = span.text();
+			span.html('<pre><span class="prefix">'+settings.prefix+'</span><span class="lp">'+initial_content+'</span><span class="cursor"> </span><span class="ln"></span></pre>');
+			
+			// Toogle the cursor color.
+			var initToggleCursorColor = function () {
+				if (rcc_id != null) {
+					clearInterval(rcc_id);
+					$('span.cursor', span).addClass('selected');
+				}
+				rcc_id = setInterval(function(){
+					$('span.cursor', span).toggleClass('selected');
+				}, 650);
+			};
+			
+			// Add keyboard events
+			span.keypress(function (e) {
+				// Synchronous call of the event trigger
+				queue.push({
+					fn: function (e) {
+						// Call the pre operating callback
+						if (typeof settings.calback_pre_keypress == "function" && settings.calback_pre_keypress(e)) {
+							return;
+						}
+						
+						// Fetch actual values
+						var lp_content = $('span.lp', span).text(), new_lp_content = lp_content,
+							ln_content = $('span.ln', span).text(), new_ln_content = ln_content,
+							cr_content = $('span.cursor', span).text(), new_cr_content = cr_content;
+						
+						switch (e.keyCode) {
+						/**
+						 * Navigation keys.
+						 * 
+						 */
+						case 37: // Left key
+							// If there isn't any content before, don't move
+							if (lp_content == '') {
+								return;
+							}
+							
+							new_lp_content = lp_content.substring(0, lp_content.length-1);
+							new_cr_content = lp_content.substring(lp_content.length-1, lp_content.length);
+							new_ln_content = cr_content + ln_content;
+							break;
+						case 39: // Right key
+							// If there isn't any content after, don't move
+							if (ln_content == '') {
+								return;
+							}
+							
+							new_lp_content = lp_content + cr_content;
+							new_cr_content = ln_content.substring(0, 1);
+							new_ln_content = ln_content.substring(1);
+							break;
+						/**
+						 * Content deletion keys.
+						 * 
+						 */
+						case 8: // Return key
+							if (lp_content == '') {
+								return;
+							}
+							new_lp_content = lp_content.substring(0, lp_content.length-1);
+							break;
+						case 46: // Suppr key
+							if (ln_content == '') {
+								return;
+							}
+							new_cr_content = ln_content.substring(0, 1);
+							new_ln_content = ln_content.substring(1);
+							break;
+						/**
+						 * Content addition keys and other keys.
+						 * 
+						 */
+						default:
+							new_lp_content += String.fromCharCode(e.charCode);
+							break;
+							
+						}
+						
+						// Update content
+						$('span.lp', span).text(new_lp_content);
+						$('span.ln', span).text(new_ln_content);
+						$('span.cursor', span).text(new_cr_content);
+						
+						// Reset cursor color
+						initToggleCursorColor();
+						
+						// Call the post operating callback
+						if (typeof settings.calback_post_keypress == "function") {
+							settings.calback_post_keypress(e);
+						}
+					},
+			        args: [e]
+				});
+			});
+			
+			// Lunch inits
+			initToggleCursorColor();
 		});
 	};
 })(jQuery);
